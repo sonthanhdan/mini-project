27255ca3d7d71afa7ed683a50ef1c84b83189333
Son Thanh Dan <dan.son@blueberrylabs.com>
2023-10-25 23:45:03 +0700

    chore: config and create entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserData } from './user-data.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'mycsv_db',
+      username: 'postgres',
+      password: 'secret',
+      entities: [UserData],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
